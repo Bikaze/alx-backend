ef3144b699eb71e2ce4d7cbd339418c05f7c00ce
Bikaze <clmntmugisha@gmail.com>
2024-07-30 10:35:31 +0200

    Create 1-app.py & 1-index.html

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module initializes a Flask application
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Renders the index page.
+
+    Returns:
+        str: The rendered HTML template as a string.
+    """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+This module initializes a Flask application with Babel for localization
+support.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+# Create Flask application
+app = Flask(__name__)
+
+
+# Configure Babel
+class Config:
+    """
+    Configuration class for Babel settings.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Renders the index page.
+
+    Returns:
+        str: The rendered HTML template as a string.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
